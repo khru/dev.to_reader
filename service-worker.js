550ce0fd79fa75cf4cb6955478829ddac3210fcd
Emmanuel Valverde Ramos <evrtrabajo@gmail.com>
2021-06-22 08:05:14 +0200

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,24 @@
+self.addEventListener('install', e => {
+ e.waitUntil(
+    caches.open('airhorner').then(cache => {
+        return cache.addAll([
+            '/',
+            'dev.to/',
+            'res.cloudinary.com/'
+        ])
+            .then(() => self.skipWaiting());
+    })
+  )
+});
+
+self.addEventListener('activate', event => {
+ event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+        return response || fetch(event.request);
+    })
+  );
+});
